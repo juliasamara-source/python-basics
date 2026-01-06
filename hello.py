b9dd85ba35d3129e7888cf8abb8f969c2f59bb71
juliasamara-source <juliasquaresmara@gmail.com>
2026-01-06 16:56:19 -0300

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,12 @@
+hello.py
+nome = input("Qual é o seu nome? ")
+cidade = input("De onde você é? ")
+idade = input("Quantos anos você tem? ")
+
+print("Oi", nome)
+print("Você é de", cidade)
+print("Você tem", idade, "anos")
+ 
+
+
+ 
